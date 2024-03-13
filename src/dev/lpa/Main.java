1ddc88a42bb3f316007c924dfc2d87320c65ba40
adamd <adam.d.hinton@gmail.com>
2024-03-13 14:50:02 -0400

    Make list of fake EEs

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -5,15 +5,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
         List<Employee> employeeList = new ArrayList<>();
 
+        employeeList.add(new Employee("Bob", "Bobson", "01/13/2021"));
+        employeeList.add(new Employee("CableBob", "Cabelson", "04/13/2024"));
+        employeeList.add(new Employee("Beep Boop", "123", "12/12/1990"));
+        employeeList.add(new Employee("Please Promote Me", "Or I'll Leave", "12/12/1993"));
+        employeeList.add(new Employee("Just here for paycheck", "And Coffee", "09/19/2020"));
     }
-
-    public record Employee(String firstName, String lastName, String hireDate){
-
-    }
-
-
 }
 
+record Employee(String firstName, String lastName, String hireDate) {
+}
