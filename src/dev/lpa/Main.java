7f0f3dc67d21bd3974e93df6dd1476511564a329
adamd <adam.d.hinton@gmail.com>
2024-03-13 15:05:06 -0400

    Successfully printing employees. Now to sort.

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,5 +1,6 @@
 package dev.lpa;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +13,46 @@ public class Main {
         employeeList.add(new Employee("Beep Boop", "123", "12/12/1990"));
         employeeList.add(new Employee("Please Promote Me", "Or I'll Leave", "12/12/1993"));
         employeeList.add(new Employee("Just here for paycheck", "And Coffee", "09/19/2020"));
+
+        printEmployees(employeeList);
+
     }
+
+    public static void printEmployees(List<Employee> employeeList){
+
+        // print EEs
+
+        // local class
+        class InternalEmployee{
+            private Employee myEmployee;
+            private String fullName;
+            private int yearsWorked;
+
+            public InternalEmployee(Employee myEmployee) {
+                this.myEmployee = myEmployee;
+                this.fullName = myEmployee.firstName() + " " + myEmployee.lastName();
+
+                int currentYear = LocalDate.now().getYear();
+                // Last four digits, aka yearstarted
+                int yearStarted = Integer.parseInt(myEmployee.hireDate().substring(myEmployee.hireDate().length() -4));
+                this.yearsWorked = currentYear - yearStarted;
+            }
+
+            @Override
+            public String toString() {
+                return "InternalEmployee{" +
+                        ", fullName='" + fullName + '\'' +
+                        ", yearsWorked=" + yearsWorked +
+                        '}';
+            }
+        }
+
+        for (var employee : employeeList){
+            System.out.println(employee);
+        }
+
+    }
+
 }
 
 record Employee(String firstName, String lastName, String hireDate) {
